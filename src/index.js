8892c610f5b1b282242f1b57fefbb8b76bb7cc91
firedfalcon <firedfalcon@gmail.com>
2022-11-22 16:06:42 -0300

    base build

---
+++ src/index.js
@@ -0,0 +1,27 @@
+﻿
+'use strict';
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path');
+var cors = require('cors');
+const app = express()
+const port = 3000
+
+app.set('port', process.env.port || port);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Headers
+app.use(cors());
+
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
---
+++ src/libs/database.js
@@ -0,0 +1,18 @@
+﻿const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+})
+
+pool.connect()
+    .then(pool => {
+        console.log('Connected to Supabase...')
+        return pool
+    })
+    .catch(err => console.log('Database Connection Failed!: ', err));
+
+module.exports = pool;
